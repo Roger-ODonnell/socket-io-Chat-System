025251f1c4f9029df66d9d870a76647ddc1367e4
Unfixingdock <71945647+Roger-ODonnell@users.noreply.github.com>
2023-03-20 15:10:09 +0000

    removed duplicate code

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  
-});
-
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
@@ -21,12 +17,6 @@ io.on('connection', (socket) => {
   io.emit("chat message", "User Connected");
   socket.on('disconnect', () => {
     io.emit("chat message", "User disconnected");
-  });
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
   });
 });
 
