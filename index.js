45bd7c8c247eddd907e44749db1aa15afcf0e159
Unfixingdock <71945647+Roger-ODonnell@users.noreply.github.com>
2023-03-20 15:12:13 +0000

    updated index

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
 
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   io.emit("chat message", "User Connected");
-  socket.on('disconnect', () => {
-    io.emit("chat message", "User disconnected");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
   });
-});
-
-io.on('connection', (socket) => {
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+  socket.on("disconnect", () => {
+    io.emit("chat message", "User disconnected");
   });
 });
